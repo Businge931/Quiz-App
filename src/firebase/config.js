e8200fa5f9bed1f1af5eac1ff15910d28d2c2a72
Businge931 <jimmybusinge9@gmail.com>
2023-04-14 23:48:04 +0300

    feat: edit firebase credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBufAmfPzPURBAXr61Iwq_WsWw71wGQ26k",
+  authDomain: "quiz-app-65f53.firebaseapp.com",
+  projectId: "quiz-app-65f53",
+  storageBucket: "quiz-app-65f53.appspot.com",
+  messagingSenderId: "540276434353",
+  appId: "1:540276434353:web:5bd413ba0f893e15edc990",
 };
 
 initializeApp(firebaseConfig);
